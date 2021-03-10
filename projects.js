cd902fecba61fe22cf1634052aa01b4758702d84
Vaidehi <vaidehisonar@gmail.com>
2021-03-10 10:00:00 -0500

    Initial Website

---
+++ projects.js
@@ -0,0 +1,54 @@
+
+function addinfo1(){
+    document.getElementById("proj-name").innerHTML = "Forest Cover Type Prediction";
+    document.getElementById("info").innerHTML = "Enhanced the accuracy by 10% using dimensionality reduction and feature selection.Fine-tuned the models using bagging for PCA and LassoCV.Built models like K-NN, Decision Trees, Logistic Regression with highest accuracy for Voting Classifier.";
+}
+
+function addinfo2(){
+    document.getElementById("proj-name").innerHTML = "Loss Ratio Prediction for Auto Insurance";
+    document.getElementById("info").innerHTML = "Performed statistical modeling, data pre-processing and data wrangling.Boosted accuracy by 30% using feature engineering(aggregation) and models- XG-Boost, neural networks.Reduced RMSE by 0.70 using Random forest.";
+}
+
+function addinfo3(){
+    document.getElementById("proj-name").innerHTML = "Tableau Data Science Jobs";
+    document.getElementById("info").innerHTML = "Worked on a Tableau Dashboard to find insights regarding Data science Jobs. Answered questions like how is the salary estimate in different locations in United States and in different industries and sectors? Which are the popular jobs in data and what is the hiring trend?";
+}
+
+function addinfo4(){
+    document.getElementById("proj-name").innerHTML = "Using Satellite Images to predict GDP of Countries";
+    document.getElementById("info").innerHTML = "Improved the GDP prediction using RMSE of a mixed model – MLP for numerical and CNN for image data. Pre-processed the data - Data Augmentation, resizing images to improve performance.Explained the model predictions with the help of filters.";
+}
+
+function addinfo5(){
+    document.getElementById("proj-name").innerHTML = "Tableau COVID-19 in USA";
+    document.getElementById("info").innerHTML = "Worked on COVID-19 Dataset to analyze and visualize how is COVID-19 is spreading in USA. What is the testing rate, infection rate and the death rate? Tried to understand if factors like age, temperature, pollution, population are affecting COVID-19 spread? If employment, unemployment or other financial and health spending factors affect its spread?";
+}
+
+function addinfo7(){
+    document.getElementById("proj-name").innerHTML = "SENSORIUM: A Bot for Human Sensory System";
+    document.getElementById("info").innerHTML = "Designed a prototype of an integrated system that can speak, listen, detect and classify objects, smell gases and sense temperature by establishing contact. Stimulated how the human brain coordinates the 5 senses using AI. Project Technologies: Python, OpenCV, Deep learning, Raspberry pi and sensors, Speech recognition, Natural Language Processing (NLTK). ";
+}
+
+
+
+
+
+function addinfo6(){
+    document.getElementById("proj-name1").innerHTML = "Dice Webpage";
+    document.getElementById("info1").innerHTML = "Just a fun Dice game web page where a random dice is thrown and the player with higher number wins or if they are equal then its a draw.";
+}
+
+function addinfo8(){
+    document.getElementById("proj-name1").innerHTML = "GoShop Web Application";
+    document.getElementById("info1").innerHTML = "A shopping guide to help people discover the places for shopping around them. Features: Searching shops, viewing shop details like location, timings, collections, filtering, provide review and ratings, etc. Complete software development life cycle. Technology used : HTML, CSS, JavaScript, React JS, RestAPI, MySQL";
+}
+
+
+/*
+function addcode(){
+var mydiv = document.getElementById("info-container");
+var mycontent = document.createElement("p");
+mycontent.appendChild(document.createTextNode("This is a paragraph"));
+mydiv.appendChild(mycontent);
+}
+*/
